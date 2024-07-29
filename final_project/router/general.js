4f3135163a0648a559cdc6daa642f244ee6a1f5c
codehomie1 <mogpt2x@gmail.com>
2024-07-29 16:18:06 -0400

    convert general routes into promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,43 +23,69 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4));
+  let bookPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve(JSON.stringify(books, null, 4));
+    }, 1000);
+  });
+
+  const data = await bookPromise;
+  res.send(data);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
-  let isbn = req.params.isbn;
-  return res.send(books[isbn]);
- });
+  const isbn = req.params.isbn;
+  let get_details = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(books[isbn]);
+    }, 1000)
+  })
+    
+  let data = await get_details;
+  res.send(data);
+
+ });    
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   //Write your code here
-  books_result = []
-  Object.keys(books).forEach(id => {
-    if(books[id].author === req.params.author) {
-        books_result.push(books[id])
-    }
+  const get_details = new Promise((resolve, reject) => {
+    books_result = []
+    Object.keys(books).forEach(id => {
+      if(books[id].author === req.params.author) {
+          books_result.push(books[id])
+      }
+    });
+    resolve(books_result);
   });
-  res.send(books_result)
+
+  let data = await get_details;
+  res.send(data);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  let books_result = [];
+  let book_details = new Promise((resolve, reject) => {
+    const title = req.params.title;
+    let books_result = [];
+  
+    Object.keys(books).forEach(id => {
+      if(books[id].title === title) {
+          books_result.push(books[id])
+      }
+    });
 
-  Object.keys(books).forEach(id => {
-    if(books[id].title === title) {
-        books_result.push(books[id])
-    }
-  });
+    resolve(books_result);
+  })
 
-  res.send(books_result);
+
+  let data = await book_details;
+  res.send(data);
 });
 
 //  Get book review
